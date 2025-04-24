23ad6806985c0326412113db95856fa0b56a5a22
donquidofi612 <168350352+donquidofi612@users.noreply.github.com>
2025-04-24 17:55:12 +0000

    Update maindashboard.vue

--- layouts/maindashboard.vue
+++ layouts/maindashboard.vue
@@ -10,7 +10,7 @@
               <!-- Start::page-header -->
               <div class="d-md-flex d-block align-items-center justify-content-between my-4 page-header-breadcrumb">
                 <div>
-                    <p class="fw-semibold fs-18 mb-0">Bon retour {{fullname}}!</p>
+                    <p class="fw-semibold fs-18 mb-0"> {{fullname}}!</p>
                     <span class="fs-semibold text-muted">Dernière connexion: {{currentDateTime}}.</span>
                   <p class="fw-semibold text-danger mb-0">Compte séquestré pour procédure inter-administratif</p>
                 </div>
