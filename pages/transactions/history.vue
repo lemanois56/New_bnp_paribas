74b06a0b641ce9a5e663177ed0a150558d03eb3f
donquidofi612 <168350352+donquidofi612@users.noreply.github.com>
2025-04-28 19:39:25 +0000

    Update history.vue

--- pages/transactions/history.vue
+++ pages/transactions/history.vue
@@ -93,7 +93,7 @@ export default {
     const transactions = ref([
       { id: 1, date: '23/04/2025', description: 'Virement reçu', type: 'Crédit', amount: 12000, balanceAfter: 12000 },
       { id: 2, date: '25/04/2025', description: 'Virement reçu', type: 'Crédit', amount: 42000, balanceAfter: 30000 },
-     // { id: 3, date: '30/09/2024', description: 'Achat Boutique', type: 'Débit', amount: -200, balanceAfter: 8000 },
+      { id: 3, date: '25/04/2025', description: 'Virement reçu', type: 'Crédit', amount: 24000, balanceAfter: 66000 },
      // { id: 4, date: '29/09/2024', description: 'Virement', type: 'Crédit', amount: 500, balanceAfter: 8200 },
      // { id: 5, date: '28/09/2024', description: 'Retrait DAB', type: 'Débit', amount: -1000, balanceAfter: 7700 },
       // Ajouter plus de transactions pour illustrer l'historique complet
