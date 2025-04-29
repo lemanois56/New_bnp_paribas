6951c904454867450131a50ba96a92a46c67dacc
donquidofi612 <168350352+donquidofi612@users.noreply.github.com>
2025-04-29 15:10:43 +0000

    Update maindashboard.vue

--- layouts/maindashboard.vue
+++ layouts/maindashboard.vue
@@ -12,7 +12,7 @@
                 <div>
                     <p class="fw-semibold fs-18 mb-0"> {{fullname}}!</p>
                     <span class="fs-semibold text-muted">Dernière connexion: {{currentDateTime}}.</span>
-                  <p class="fw-semibold text-danger mb-0">Compte séquestre pour procédure inter-administratif</p>
+                  <p class="fw-semibold text-success mb-0">Compte actif</p>
                 </div>
                 <div class="btn-list mt-md-0 mt-2">
                     <button type="button" class="btn btn-primary btn-wave">
